dc51b5c60e7a3b1ddf3b9bc3f444e90e1b22e7cf
Ann Yuan <ayuan@thisisgrow.com>
2015-05-29 06:47:45 -0400

    update room counts

--- source/assets/js/room.js
+++ source/assets/js/room.js
@@ -3,7 +3,6 @@ var sw = require('./socket');
 
 module.exports.initialize = function() {
   sw.socket.on('user update', function(data) {
-    console.log("USER UPDATE");
-    console.log(data);
+    document.querySelector('.room-count').innerHTML = data.count;
   });
 };
