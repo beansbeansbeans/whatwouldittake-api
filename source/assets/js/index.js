38efac101dc15bbea3be06ed5c457d501d7fbe64
Ann Yuan <ayuan@thisisgrow.com>
2015-05-29 07:08:11 -0400

    make stuff linkable

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -3,10 +3,8 @@ var sw = require('./socket');
 
 module.exports.initialize = function() {
   sw.socket.on('rooms update', function(data) {
-    var template = data.reduce(function(prev, curr) {
+    document.querySelector('.rooms').innerHTML = data.reduce(function(prev, curr) {
       return prev + util.processTemplate(curr, 'room_partial');
     }, '');
-
-    document.querySelector('.rooms').innerHTML = template;
   });
 };
