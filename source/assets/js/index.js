c7429ff8380a5bd31e4d3cdde649ca39b36e9821
Ann Yuan <ayuan@thisisgrow.com>
2015-05-30 14:46:48 -0400

    adjustments

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -5,6 +5,12 @@ module.exports.initialize = function() {
   sw.socket.on('rooms update', function(data) {
     d.qs('body').classList.remove('loading');
 
+    if(data.length) {
+      d.qs('body').setAttribute("data-mode", "occupied");
+    } else {
+      d.qs('body').setAttribute("data-mode", "empty");
+    }
+
     d.qs('.rooms').innerHTML = data.reduce(function(prev, curr) {
       return prev + util.processTemplate(curr, 'room_partial');
     }, '');
