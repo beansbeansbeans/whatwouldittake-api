b1dae801fa705f6b6a8de1b7013fed50902340ca
Ann Yuan <ayuan@thisisgrow.com>
2015-05-30 09:56:02 -0400

    add loader display logic

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -3,6 +3,8 @@ var sw = require('./socket');
 
 module.exports.initialize = function() {
   sw.socket.on('rooms update', function(data) {
+    document.querySelector('body').classList.remove('loading');
+
     document.querySelector('.rooms').innerHTML = data.reduce(function(prev, curr) {
       return prev + util.processTemplate(curr, 'room_partial');
     }, '');
