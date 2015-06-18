7f0c1fedde75c26c70c022661567e252aca3e5fd
Ann Yuan <ayuan@thisisgrow.com>
2015-06-18 06:52:09 -0400

    basic grid

--- source/assets/js/room/gridHelpers.js
+++ source/assets/js/room/gridHelpers.js
@@ -1,9 +1,11 @@
 var gridCount;
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
 
 var updateGrid = (chatterCount) => {
   if(!gridCount) { gridCount = 0; }
   
-  gridCount = Math.ceil(2 * chatterCount);
+  gridCount = Math.max(10, Math.ceil(2 * chatterCount));
 };
 
 module.exports = {
@@ -13,9 +15,39 @@ module.exports = {
       || data < 0.5 * gridCount) {
       updateGrid(data);
     }
-
   },
   getGridCount() {
     return gridCount;
+  },
+  getSquareSize() {
+    return Math.round(windowWidth * windowHeight / gridCount);
+  },
+  getDownFromAcross (across) {
+    return Math.ceil(windowWidth * windowHeight / across / this.getSquareSize());
+  },
+  getGridOrientation() {
+    var area = windowWidth * windowHeight,
+      widthOverHeight = windowWidth / windowHeight,
+      across = Math.ceil(Math.sqrt(area) / this.getSquareSize()),
+      down = this.getDownFromAcross(across);
+    
+    if(across / down < widthOverHeight) {
+      while( across / down < widthOverHeight) {
+        across++;
+        down = this.getDownFromAcross(across);
+      }
+      down++;
+    } else {
+      while( across / down > widthOverHeight) {
+        across--;
+        down = this.getDownFromAcross(across);
+      }
+      across++;
+    }
+
+    return {
+      across: across,
+      down: down
+    };
   }
 };
