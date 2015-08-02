09dc348d0bac181a62a6797434ea762223700bb2
Ann Yuan <ayuan@thisisgrow.com>
2015-08-02 12:17:51 -0400

    liberal

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ function Routes (app, ee) {
   var scenariosDB = client.collection('scenarios');
 
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     next();
